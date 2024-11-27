0b611768c9101c60028c178a9700aca254603328
Megalomaniac Schtkan <texview@gmx.com>
2024-11-27 22:29:47 +0300

    Charts init

---
+++ Configastic/Components/Features/Scanner/Pages/ViewModels/InputViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Configastic.SharedModels.Interfaces;
+using Configastic.SharedModels.Models.BolidDevices.ElectricModules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Configastic.Components.Features.Scanner.Pages.ViewModels
+{
+    public class InputViewModel
+    {
+        private Shleif _input;
+
+        public int InputIndex { get; set; }
+
+        public InputViewModel(Shleif input)
+        {
+            _input = input;
+            InputIndex = _input.ShleifIndex;
+        }
+    }
+}
